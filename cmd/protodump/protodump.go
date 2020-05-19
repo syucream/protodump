0c1e54c6c3e1a5924d82f65fc3ac947210066d98
Ryo Okubo <syucream1031@gmail.com>
2020-05-19 23:55:53 +0900

    Enable to print a processed message forcely

--- cmd/protodump/protodump.go
+++ cmd/protodump/protodump.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,13 +11,26 @@ import (
 	"github.com/syucream/protodump"
 )
 
+func toJson(msg protodump.Message) (string, error) {
+	jsonStr, err := json.Marshal(msg)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonStr), nil
+}
+
 func main() {
+	force := flag.Bool("force", false, "force to print on error")
+	flag.Parse()
+
+	args := flag.Args()
+
 	var in []byte
 	var err error
-
-	if len(os.Args) >= 2 {
+	if len(args) >= 1 {
 		// filename by arg1
-		in, err = ioutil.ReadFile(os.Args[1])
+		in, err = ioutil.ReadFile(args[0])
 	} else {
 		// stdin
 		in, err = ioutil.ReadAll(os.Stdin)
@@ -27,14 +41,15 @@ func main() {
 
 	msg := protodump.Message{}
 	err = protodump.Unmarshal(in, msg)
+
+	if err == nil || *force {
+		jsonStr, err := toJson(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(jsonStr)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	jsonStr, err := json.Marshal(msg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(jsonStr))
 }
